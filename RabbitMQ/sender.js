236943fe7b75dda484ef6ed5298f979ea802bcd3
Sonali <sonali.singh@pluang.com>
2022-02-03 10:27:08 +0530

    Send message to RabbitMQ

---
+++ RabbitMQ/sender.js
@@ -0,0 +1,23 @@
+import amqp from "amqplib/callback_api"
+
+let ch = null
+const CONN_URL = "http://localhost:15672/"
+amqp.connect(CONN_URL, (err, connection)=>{
+    if(err)
+        throw err
+
+    connection.createChannel((err, channel) => {
+        if(err) throw err;
+    })
+
+    const QUEUE = "test"
+    channel.assertQueue(QUEUE)
+
+    channel.sendToQueue(QUEUE, Buffer.from("Message present in RabbitMQ"))
+    console.log("message sent")
+})
+
+process.on ('exit', () =>{
+    ch.close()
+    console.log("clossing rabbitmq channel")
+}) 
